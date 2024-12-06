d2cb120ed26af0a146f45d9387663d2d9321db78
dckap <you@example.com>
2024-12-06 22:09:09 +0530

    upload files

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 function calculateLove() {
+    let name1 = document.getElementById('name1').value.trim();
+    let name2 = document.getElementById('name2').value.trim();
 
-   let name1 = document.getElementById('name1').value.trim();
-   let name2 = document.getElementById('name2').value.trim();
-
-    if(name1 === "" || name2 === ""){
-        alert("Plz Enter Boath Names !");
+    if (name1 === "" || name2 === "") {
+        alert("Please Enter Both Names!");
         return;
     }
 
-    let nameAdd =name1+name2;
+    let nameAdd = name1 + name2;
     let loveScore = 0;
 
-        for(let i=0; i<nameAdd.length; i++){
-            loveScore += nameAdd.charCodeAt(i);
-        }
+    for (let i = 0; i < nameAdd.length; i++) {
+        loveScore += nameAdd.charCodeAt(i);
+    }
 
-        loveScore =(loveScore % 100) + 1;
+    loveScore = (loveScore % 100) + 1;
 
-        let result = document.getElementById('result');
-        result.innerHTML = `Your Love Score is ${loveScore}%`; 
+    let result = document.getElementById('result');
+    result.innerHTML = `Your Love Score is ${loveScore}%`;
 }
 
-document.getElementById('calculateBtn').addEventListener('click'.calculateLove());
+function refreshPage() {
 
-
+    document.getElementById('name1').value = '';
+    document.getElementById('name2').value = '';
+    document.getElementById('result').innerHTML = 'Result';
+}
