cf80c96bb226711ebe799f4f71d97e635a5136cc
naveenkumar-kanniyappan <naveenkumarkdckap@gmail.com>
2025-01-22 15:48:15 +0530

    add files

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
 function calculateLove() {
     let name1 = document.getElementById('name1').value.trim();
     let name2 = document.getElementById('name2').value.trim();
-
+    
     if (name1 === "" || name2 === "") {
-        alert("Please Enter Both Names!");
+        alert("Please enter both names!");
         return;
     }
-
-    let nameAdd = name1 + name2;
-    let loveScore = 0;
-
-    for (let i = 0; i < nameAdd.length; i++) {
-        loveScore += nameAdd.charCodeAt(i);
+    
+    name1 = name1.toLowerCase();
+    name2 = name2.toLowerCase();
+    
+    let arr1 = name1.split('');
+    let arr2 = name2.split('');
+    
+    for (let i = 0; i < arr1.length; i++) {
+        let index = arr2.indexOf(arr1[i]);
+        if (index !== -1) {
+            arr2.splice(index, 1);
+            arr1[i] = '';
+        }
+    }
+  
+    let remainingCount = arr1.filter(char => char !== '').length + arr2.length;
+  
+    let flames = "FLAMES";
+    let i = 0;
+    while (flames.length > 1) {
+        i = (i + remainingCount - 1) % flames.length;
+        flames = flames.slice(0, i) + flames.slice(i + 1);
+    }
+  
+    let resultText = "";
+    switch (flames) {
+        case 'F': resultText = "Friends"; break;
+        case 'L': resultText = "Lovers"; break;
+        case 'A': resultText = "Affection"; break;
+        case 'M': resultText = "Marriage"; break;
+        case 'E': resultText = "Enemies"; break;
+        case 'S': resultText = "Siblings"; break;
     }
 
-    loveScore = (loveScore % 100) + 1;
-
-    let result = document.getElementById('result');
-    result.innerHTML = `Your Love Score is ${loveScore}%`;
+    document.getElementById('result').innerHTML = `Your relationship status: ${resultText}`;
 }
 
 function refreshPage() {
-
     document.getElementById('name1').value = '';
     document.getElementById('name2').value = '';
     document.getElementById('result').innerHTML = 'Result';
